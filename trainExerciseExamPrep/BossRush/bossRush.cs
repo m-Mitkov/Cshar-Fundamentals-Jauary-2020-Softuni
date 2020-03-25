457567bc3d0aa396ffd98d0fe53fe5f41c0f95e7
Martin Mitkov <m.mitkov07@gmail.com>
2020-03-25 21:17:35 +0100

    trainExerciseExamPrep/

---
+++ trainExerciseExamPrep/BossRush/bossRush.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace bossRush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex decryptingPattern = new Regex(@"\|([A-Z]{4,})\|:(#)([A-Za-z]+ [A-Za-z]+)\2");
+                                   // group 1 - name, group 2 - '#', group 3 - title    
+            
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string currentBoss = Console.ReadLine();
+
+                if (decryptingPattern.IsMatch(currentBoss))
+                {
+                    string name = decryptingPattern.Match(currentBoss).Groups[1].ToString();
+                    string title = decryptingPattern.Match(currentBoss).Groups[3].ToString();
+
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armour: {title.Length}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
---
+++ trainExerciseExamPrep/HeroRecruitment/heroRecruitment.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace heroRecruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroes = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] comand = Console.ReadLine().Split(" ");
+
+                string toDo = comand[0];
+
+                if (toDo == "End")
+                {
+                    break;
+                }
+
+                string currentHeroName = comand[1];
+
+                if (toDo == "Enroll")
+                {
+                    if (!heroes.ContainsKey(currentHeroName))
+                    {
+                        heroes.Add(currentHeroName, new List<string>());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{currentHeroName} is already enrolled.");
+                    }
+                }
+
+                else if (toDo == "Learn")
+                {
+                    string spell = comand[2];
+
+                    if (!heroes.ContainsKey(currentHeroName))
+                    {
+                        Console.WriteLine($"{currentHeroName} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (heroes[currentHeroName].Contains(spell))
+                        {
+                            Console.WriteLine($"{currentHeroName} has already learnt {spell}.");
+                        }
+                        else
+                        {
+                            heroes[currentHeroName].Add(spell);
+                        }
+                    }
+                }
+
+                else if (toDo == "Unlearn")
+                {
+                    string spell = comand[2];
+
+                    if (!heroes.ContainsKey(currentHeroName))
+                    {
+                        Console.WriteLine($"{currentHeroName} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (!heroes[currentHeroName].Contains(spell))
+                        {
+                            Console.WriteLine($"{currentHeroName} doesn't know {spell}.");
+                        }
+                        else
+                        {
+                            heroes[currentHeroName].Remove(spell);
+                        }
+                    }
+                }
+            }
+
+            heroes = heroes
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine("Heroes:");
+
+            foreach (var hero in heroes)
+            {
+                Console.WriteLine($"== {hero.Key}: {string.Join(", ", hero.Value)}");
+            }
+        }
+    }
+}
