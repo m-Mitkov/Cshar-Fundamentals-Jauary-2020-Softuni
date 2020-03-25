936260c113197b84164e548a028b527c9e6a93f5
Martin Mitkov <m.mitkov07@gmail.com>
2020-03-25 15:23:31 +0100

    Extract emails

---
+++ regularExpressions/extractEmails/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace extractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex emailExtractor = new Regex(@"(\s[a-z]+[\w.-]+\w)@([a-z]+[-a-z]*?([.][a-z]+)+)\b");
+
+            string[] input = Console.ReadLine()
+                .Split(" ");
+
+            List<string> validEmails = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (emailExtractor.IsMatch(input[i].ToString()))
+                {
+                    validEmails.Add(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validEmails));
+        }
+    }
+}
---
+++ regularExpressions/extractEmails/extractEmails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace extractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex emailExtractor = new Regex(@"(\s[a-z]+[\w.-]+\w)@([a-z]+[-a-z]*?([.][a-z]+)+)\b");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(string.Join(Environment.NewLine,emailExtractor.Matches(input)));
+        }
+    }
+}
